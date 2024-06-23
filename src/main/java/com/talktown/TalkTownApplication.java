069ec97bb0c13d20c753e43295aded6847821271
Phan Phat Tai <taib2014946@student.ctu.edu.vn>
2024-06-23 20:42:55 +0700

    feat: setup connection PostgreSQL database

--- src/main/java/com/talktown/TalkTownApplication.java
+++ src/main/java/com/talktown/TalkTownApplication.java
@@ -2,11 +2,19 @@ package com.talktown;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class TalkTownApplication {
 
     public static void main(String[] args) {
+        // Load .env file
+        Dotenv dotenv = Dotenv.configure().load();
+        // Set environment variables
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("jwt.secret", dotenv.get("TOKEN"));
         SpringApplication.run(TalkTownApplication.class, args);
     }
 
