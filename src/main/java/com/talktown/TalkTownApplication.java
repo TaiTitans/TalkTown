f7bf16033302d8947b1d343bc88268645f19bb8c
Phan Phat Tai <taib2014946@student.ctu.edu.vn>
2024-06-23 15:14:04 +0700

    feat: setup project SpringBoot

---
+++ src/main/java/com/talktown/TalkTownApplication.java
@@ -0,0 +1,13 @@
+package com.talktown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkTownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalkTownApplication.class, args);
+    }
+
+}
